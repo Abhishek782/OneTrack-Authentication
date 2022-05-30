33f87e52bec75b765f6428724eb19de0f7e921c4
AyushWadalkar <ayushwadalkar22@gmail.com>
2022-05-30 11:08:13 +0530

    UI done

---
+++ lib/Screens/Routine_pages/list_items.dart
@@ -0,0 +1,16 @@
+import 'list_item.dart';
+
+final List<ListItem> listItems = [
+  ListItem(title: "Study karana padega na bhai",
+      urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "Attending the live meet",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work1",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work1",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work3",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work4",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work5",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work6",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work7",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work8",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+  ListItem(title: "work9",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
+];
