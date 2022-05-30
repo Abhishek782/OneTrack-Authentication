953585368408a72bc595a6c27a0309811fe20569
Sarthak Deshmukh <sarthakd1512@gmail.com>
2022-05-30 12:37:51 +0530

    it60

--- lib/Screens/Routine_pages/list_items.dart
+++ lib/Screens/Routine_pages/list_items.dart
@@ -1,11 +1,11 @@
 import 'list_item.dart';
 
 final List<ListItem> listItems = [
-<<<<<<< HEAD
+// <<<<<<< HEAD
+  // ListItem(title: "work 10",
+// =======
   ListItem(title: "work 10",
-=======
-  ListItem(title: "work 10",
->>>>>>> 18423f25769daa7e59ec17e023b5f0d599370bda
+// >>>>>>> 18423f25769daa7e59ec17e023b5f0d599370bda
       urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
   ListItem(title: "Attending the live meet",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
   ListItem(title: "work1",urlImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Taj_Mahal_Front.JPG/1200px-Taj_Mahal_Front.JPG?20120915083330'),
